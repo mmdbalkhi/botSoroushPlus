d78b9f304fb1fae40acfe30fcb4ef7b59e616339
KomeilParseh <ahmdparsh@gmail.com>
2021-02-03 19:50:04 +0330

    move all *.py to app + add goodreads model to app + send massage

---
+++ app/bot.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+from sys import path
+
+from requests import get
+
+from config import bot_token
+from sdk_bot.client import Client
+from get_book_name import get_book_name
+
+
+bot = Client(bot_token)
+try:
+    messages = bot.get_messages()
+    for message in messages:
+        print("New Message Received: " + str(message))
+        #text = get_book_name(message[""])
+        #bot.send_text(message["from"], text)
+except Exception as e:
+    print(e.args[0])
