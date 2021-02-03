20e7548b4bb83f89aa631f5ce221d910daa77773
KomeilParseh <ahmdparsh@gmail.com>
2021-02-03 23:08:58 +0330

    change name bot to main

--- app/main.py
+++ app/main.py
@@ -4,17 +4,20 @@ from sys import path
 from requests import get
 
 from config import bot_token
-from sdk_bot.client import Client
 from get_book_name import get_book_name
+from sdk_bot.client import Client
 
-
-text = get_book_name()
 bot = Client(bot_token)
 try:
     messages = bot.get_messages()
     for message in messages:
         print("New Message Received: " + str(message))
-        to = message["from"]
-        bot.send_text(to, text)
+
+        send_text = get_book_name(message["body"])
+
+        if send_text == "این کتاب ها را یافتیم!\n\n":
+            send_text = "چیزی نیافتیم! لطفا در نوشتار کلمه دقت کنید! "
+
+        bot.send_text(message["from"], send_text)
 except Exception as e:
     print(e.args[0])
