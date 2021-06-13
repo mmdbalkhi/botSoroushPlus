3fc4641f2a4bd55f692d653a42a93051eb72c065
Komeil Parseh <65954744+KomeilParseh@users.noreply.github.com>
2021-06-13 17:01:01 +0430

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,21 +5,23 @@
 from client import Client, get_book_name
 
 from config import BOT_TOKEN
-START_TEXT = """سلام به ربات کتاب خوب خوش آمدید شما میتوانید نام نویسنده یا کتاب مورد نظرتون رو جست و جو کنید تا بهترین نتایج به دست شما برسه:) """
+START_TEXT = """سلام به ربات کتاب خوب خوش آمدید شما میتوانید نام نویسنده \
+یا کتاب مورد نظرتون رو جست و جو کنید تا بهترین نتایج به دست شما برسه:) """
 
 bot = Client(BOT_TOKEN)
 try:
     messages = bot.get_messages()
     for message in messages:
         print("New Message Received: " + str(message))
+        user_text = message["body"]
         if message["type"] == "START":
             bot.send_text(message["from"], START_TEXT)
 
         elif message["type"] == "TEXT":
-            SEND_TEXT = get_book_name(message["body"])
+            SEND_TEXT = get_book_name(user_text)
 
-            if SEND_TEXT == "برای کلیدواژه «الپاچینو» این کتاب(ها)را یافتیم.\n\n":
-                SEND_TEXT = "چیزی نیافتیم! لطفا در نوشتار کلمه دقت کنید! "
+            if SEND_TEXT == f"برای کلیدواژه «{user_text}» این کتاب(ها)را یافتیم.\n\n":
+                SEND_TEXT = f"برای کلیدواژه «{user_text}» چیزی نیافتیم. لطفا در نوشتار دقت کنید."
 
             bot.send_text(message["from"], SEND_TEXT)
         else:
